3247c4412427649a4741dac6980f4923dd7d0c00
k-koech <kelvin.koech@student.moringaschool.com>
2022-08-24 22:43:12 +0300

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,9 @@
 
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
+# Require statements should go here.
+
+# Then any files that need to load the files required here can
+# require THIS file, environment.rb, and get access to everything
+require_relative '../lib/garden'
+require_relative '../lib/plant'
